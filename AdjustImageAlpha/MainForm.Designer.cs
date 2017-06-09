f2190a6e528931709264c52f74d0140daed97b45
Keith Liu <lzhsjster@msn.com>
2017-06-09 23:03:16 +0800

    Initial commit

---
+++ AdjustImageAlpha/MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace AdjustImageAlpha
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+        private System.Windows.Forms.TrackBar trackBarImage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            this.trackBarImage = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 36);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Drag the sliders to apply the specified opacity value to the respective image. Va" +
+    "lue range: 0 - 100%";
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxImage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxImage.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBoxImage.Location = new System.Drawing.Point(8, 16);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(305, 152);
+            this.pictureBoxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxImage.TabIndex = 6;
+            this.pictureBoxImage.TabStop = false;
+            this.pictureBoxImage.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBoxImage_DragDrop);
+            this.pictureBoxImage.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBoxImage_DragEnter);
+            this.pictureBoxImage.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxImage_Paint);
+            // 
+            // trackBarImage
+            // 
+            this.trackBarImage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBarImage.LargeChange = 10;
+            this.trackBarImage.Location = new System.Drawing.Point(319, 2);
+            this.trackBarImage.Maximum = 100;
+            this.trackBarImage.Name = "trackBarImage";
+            this.trackBarImage.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBarImage.Size = new System.Drawing.Size(45, 175);
+            this.trackBarImage.TabIndex = 5;
+            this.trackBarImage.TickFrequency = 10;
+            this.trackBarImage.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBarImage.Value = 100;
+            this.trackBarImage.Scroll += new System.EventHandler(this.trackBarImage_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(364, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "100%";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(362, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "0%";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(380, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(8, 127);
+            this.label4.TabIndex = 12;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(320, 181);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(92, 24);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "*.png";
+            this.saveFileDialog.Filter = "PNG files|*.png|All files|*.*";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 220);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxImage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBarImage);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Alpha Changer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ AdjustImageAlpha/MainForm.cs
@@ -0,0 +1,153 @@
+﻿/*
+  +MIT License
+ +
+ +Copyright (c) 2017 LiuKeith
+ +
+ +Permission is hereby granted, free of charge, to any person obtaining a copy
+ +of this software and associated documentation files (the "Software"), to deal
+ +in the Software without restriction, including without limitation the rights
+ +to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ +copies of the Software, and to permit persons to whom the Software is
+ +furnished to do so, subject to the following conditions:
+ +
+ +The above copyright notice and this permission notice shall be included in all
+ +copies or substantial portions of the Software.
+ +
+ +THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ +IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ +FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ +AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ +LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ +OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ +SOFTWARE.
+ */
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdjustImageAlpha
+{
+    public partial class MainForm : Form
+    {
+        private Bitmap imageBitmap; // Bitmap object for the Image
+
+        protected Bitmap imageToSave;
+        private ImageAttributes imageAttributes;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            trackBarImage.Enabled = false;
+            btnSave.Enabled = false;
+
+            pictureBoxImage.AllowDrop = true;
+        }
+
+        private void pictureBoxImage_Paint(object sender, PaintEventArgs e)
+        {
+            if (imageBitmap == null) return;
+            Graphics g = e.Graphics;
+            g.Clear(Color.Transparent);
+
+            // Create a new color matrix with the alpha value set to the opacity specified in the slider
+            ColorMatrix cm = new ColorMatrix();
+            cm.Matrix00 = cm.Matrix11 = cm.Matrix22 = cm.Matrix44 = 1;
+            cm.Matrix33 = (float)trackBarImage.Value / 100; // the matrix is of the form RGBA, where the (4,4)th element rep alpha
+
+            // Create a new image attribute object and set the color matrix to the one you just created
+            if (imageAttributes == null)
+            {
+                imageAttributes = new ImageAttributes();
+            }
+            imageAttributes.SetColorMatrix(cm);
+
+            // Draw the original image with the image attributes specified
+            g.DrawImage(imageBitmap, new Rectangle(0, 0, imageBitmap.Width, imageBitmap.Height), 0, 0, imageBitmap.Width, imageBitmap.Height, GraphicsUnit.Pixel, imageAttributes);
+        }
+
+        // Whenever the user changes the value of the sliders, call the respective pictureBox's Refresh()
+        private void trackBarImage_Scroll(object sender, System.EventArgs e)
+        {
+            pictureBoxImage.Refresh();
+        }
+
+        private void LoadImage(string fileName)
+        {
+            imageBitmap = new Bitmap(fileName);
+            trackBarImage.Enabled = true;
+            btnSave.Enabled = true;
+            trackBarImage.Value = 100;
+            imageAttributes = new ImageAttributes();
+            pictureBoxImage.Refresh();
+
+        }
+
+        private void pictureBoxImage_DragDrop(object sender, DragEventArgs e)
+        {
+            if ((e.AllowedEffect & DragDropEffects.Copy) == DragDropEffects.Copy)
+            {
+                Array data = e.Data.GetData(DataFormats.FileDrop) as Array;
+                if (data != null)
+                {
+                    if ((data.Length == 1) && (data.GetValue(0) is string))
+                    {
+                        string filename = ((string[])data)[0];
+                        string ext = Path.GetExtension(filename).ToLower();
+                        if ((ext == ".jpg") || (ext == ".png") || (ext == ".bmp"))
+                        {
+                            LoadImage(filename);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void pictureBoxImage_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                SaveImageAsync(fileName);
+            }
+        }
+
+        private async void SaveImageAsync(string fileName)
+        {
+            imageToSave = await Task.Run(() => GetCurrentImage());
+
+            imageToSave.Save(fileName);
+        }
+
+        private Bitmap GetCurrentImage()
+        {
+            Bitmap output = new Bitmap(imageBitmap.Width, imageBitmap.Height);
+            using (Graphics g = Graphics.FromImage(output))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.DrawImage(imageBitmap, new Rectangle(0, 0, imageBitmap.Width, imageBitmap.Height), 0, 0, imageBitmap.Width, imageBitmap.Height, GraphicsUnit.Pixel, imageAttributes);
+            }
+
+            return output;
+        }
+    }
+}
